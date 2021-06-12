c0ba52954bb8e8a17230e36e164d2dc10748cd83
zunayedsyed <71093884+zunayedsyed@users.noreply.github.com>
2021-06-12 18:20:14 +0600

    LOVE PATTERN
    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# love pattern
+import turtle
+turtle.begin_fill()
+turtle.color("white")
+turtle.bgcolor("#FF0000")
+turtle.left(50)
+turtle.forward(100)
+turtle.circle(50,180)
+turtle.right(90)
+turtle.circle(50,180)
+turtle.forward(100)
+turtle.end_fill()
+turtle.done()
